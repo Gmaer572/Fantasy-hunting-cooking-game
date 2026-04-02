a38fd64ca383c6c785456dcf935afe2c2d8f9a67
BaituanZ <bzhou@macalester.edu>
2026-04-02 03:27:31 -0500

    update health bar within the game scene

--- Assets/Scripts/HealthBarController.cs
+++ Assets/Scripts/HealthBarController.cs
@@ -5,17 +5,49 @@ public class HealthBarController : MonoBehaviour
 {
     [SerializeField] private Image healthBarImage;
     [SerializeField] private Sprite[] healthSprites;
+    private static HealthBarController instance;
 
     private void Awake()
     {
+        if (instance != null && instance != this)
+        {
+            Canvas duplicateCanvas = GetComponentInParent<Canvas>();
+            if (duplicateCanvas != null)
+            {
+                Destroy(duplicateCanvas.gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            return;
+        }
+
+        instance = this;
+
         if (healthBarImage == null)
         {
             healthBarImage = GetComponent<Image>();
+        }
+
+        Canvas parentCanvas = GetComponentInParent<Canvas>();
+        if (parentCanvas != null)
+        {
+            DontDestroyOnLoad(parentCanvas.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
         }
     }
 
     public void UpdateHealthBar(int currentHealth)
     {
+        if (healthBarImage == null)
+        {
+            return;
+        }
+
         if (healthSprites == null || healthSprites.Length == 0)
         {
             Debug.LogWarning("Health sprites are not assigned.");
