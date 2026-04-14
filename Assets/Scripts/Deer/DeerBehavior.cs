b3d46d83b1a71c3a1474ccaf4890ec2649d7641f
BaituanZ <bzhou@macalester.edu>
2026-04-14 03:19:11 -0500

    update deer hitbox and hurtbox

--- Assets/Scripts/Deer/DeerBehavior.cs
+++ Assets/Scripts/Deer/DeerBehavior.cs
@@ -1,31 +1,59 @@
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class DeerBehavior : MonoBehaviour
 {
 
     Rigidbody2D rigidBody;
+    BoxCollider2D bodyCollider;
     SpriteRenderer spriteRenderer;
 
-    GameObject attack;
     bool turnAround;
     int speed;
     int tempSpeed;
+    bool isDead;
+
+    [Header("Combat")]
+    [SerializeField] int health = 3;
+    [SerializeField] float attackCooldown = 0.5f;
+    [SerializeField] float hitCooldown = 0.1f;
+    [SerializeField] float deadDisableDelay = 0.8f;
+
+    float nextAttackTime;
+    float nextHitTime;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        bodyCollider = GetComponent<BoxCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         speed = 1;
         tempSpeed = speed;
         turnAround = false;
+        isDead = false;
+
+        EnsureHurtBox();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isDead)
+        {
+            if (rigidBody != null)
+            {
+                rigidBody.linearVelocity = Vector2.zero;
+            }
+            return;
+        }
+
         rigidBody.linearVelocityX = speed;
         turnCheck();
+
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = Time.time < nextHitTime ? Color.red : Color.white;
+        }
 
         if (speed < 0)
         {
@@ -64,5 +92,77 @@ public class DeerBehavior : MonoBehaviour
     public bool getTurn()
     {
         return turnAround;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (isDead || Time.time < nextHitTime)
+        {
+            return;
+        }
+
+        nextHitTime = Time.time + hitCooldown;
+        health = Mathf.Max(0, health - damage);
+
+        if (health == 0)
+        {
+            isDead = true;
+            speed = 0;
+            if (rigidBody != null)
+            {
+                rigidBody.linearVelocity = Vector2.zero;
+                rigidBody.simulated = false;
+            }
+
+            Collider2D[] colliders = GetComponentsInChildren<Collider2D>(true);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                colliders[i].enabled = false;
+            }
+
+            Invoke(nameof(DisableSelf), deadDisableDelay);
+        }
+    }
+
+    public void TryDamagePlayer(PlayerController playerController, int damage)
+    {
+        if (isDead || Time.time < nextAttackTime || playerController == null)
+        {
+            return;
+        }
+
+        nextAttackTime = Time.time + attackCooldown;
+        playerController.TakeDamage(damage);
+    }
+
+    void DisableSelf()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void EnsureHurtBox()
+    {
+        DeerHurtBox existing = GetComponentInChildren<DeerHurtBox>(true);
+        if (existing != null)
+        {
+            return;
+        }
+
+        GameObject hurtboxObj = new GameObject("deer_hurtbox");
+        hurtboxObj.transform.SetParent(transform, false);
+        hurtboxObj.transform.localPosition = Vector3.zero;
+        hurtboxObj.transform.localRotation = Quaternion.identity;
+        hurtboxObj.transform.localScale = Vector3.one;
+
+        BoxCollider2D hurtbox = hurtboxObj.AddComponent<BoxCollider2D>();
+        hurtbox.isTrigger = true;
+
+        if (bodyCollider != null)
+        {
+            hurtbox.offset = bodyCollider.offset;
+            hurtbox.size = bodyCollider.size;
+        }
+
+        hurtboxObj.AddComponent<DeerHurtBox>();
     }
 }
--- Assets/Scripts/Deer/DeerHitBox.cs
+++ Assets/Scripts/Deer/DeerHitBox.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 
 public class DeerHitBox : MonoBehaviour
 {
+    [SerializeField] int damage = 1;
+    [SerializeField] float forwardOffsetMultiplier = 0.35f;
 
     Vector2 attackOffsetLeft;
     Vector2 attackOffsetRight;
 
     SpriteRenderer parentRenderer;
+    DeerBehavior deerBehavior;
+    BoxCollider2D hitboxCollider;
+    BoxCollider2D parentBodyCollider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        attackOffsetLeft = new Vector2(-0.37f, 0f);
-        attackOffsetRight = new Vector2(.21f, 0f);
         parentRenderer = GetComponentInParent<SpriteRenderer>();
+        deerBehavior = GetComponentInParent<DeerBehavior>();
+        hitboxCollider = GetComponent<BoxCollider2D>();
+        parentBodyCollider = GetComponentInParent<BoxCollider2D>();
+
+        BuildOffsetsFromBodyCollider();
         transform.localPosition = attackOffsetRight;
     }
 
@@ -22,5 +30,79 @@ public class DeerHitBox : MonoBehaviour
         transform.localPosition = parentRenderer.flipX ? attackOffsetLeft : attackOffsetRight;
     }
 
+    void BuildOffsetsFromBodyCollider()
+    {
+        // Default fallback if parent collider is missing.
+        Vector2 bodySize = new Vector2(0.54f, 0.62f);
+        float bodyCenterY = 0.32f;
+
+        if (parentBodyCollider != null)
+        {
+            bodySize = parentBodyCollider.size;
+            bodyCenterY = parentBodyCollider.offset.y;
+        }
+
+        float forwardOffset = bodySize.x * forwardOffsetMultiplier;
+        attackOffsetLeft = new Vector2(-forwardOffset, bodyCenterY);
+        attackOffsetRight = new Vector2(forwardOffset, bodyCenterY);
+
+        if (hitboxCollider != null)
+        {
+            hitboxCollider.offset = Vector2.zero;
+            hitboxCollider.size = bodySize;
+            hitboxCollider.isTrigger = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        TryDamagePlayer(collision);
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        TryDamagePlayer(collision);
+    }
+
+    void TryDamagePlayer(Collider2D collision)
+    {
+        if (deerBehavior == null)
+        {
+            return;
+        }
+
+        if (collision.CompareTag("Attack"))
+        {
+            return;
+        }
+
+        AttackController attackController = collision.GetComponent<AttackController>();
+        if (attackController == null)
+        {
+            attackController = collision.GetComponentInParent<AttackController>();
+        }
+        if (attackController != null)
+        {
+            return;
+        }
+
+        HurtBox hurtBox = collision.GetComponent<HurtBox>();
+        if (hurtBox == null)
+        {
+            hurtBox = collision.GetComponentInParent<HurtBox>();
+        }
+        if (hurtBox == null)
+        {
+            return;
+        }
+
+        PlayerController playerController = collision.GetComponent<PlayerController>();
+        if (playerController == null)
+        {
+            playerController = collision.GetComponentInParent<PlayerController>();
+        }
+
+        deerBehavior.TryDamagePlayer(playerController, damage);
+    }
 
 }
