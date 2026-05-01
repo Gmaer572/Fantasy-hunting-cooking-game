f30cfda7cd888b3fce2271d125db66bba10c571b
Winnie Shi <wshi@macalester.edu>
2026-05-01 09:58:02 -0500

    upload attack and waterfootstep sound

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -219,6 +219,7 @@ public class PlayerController : MonoBehaviour
         attackEndTime = Time.time + attackLockDuration;
         player_animator.SetTrigger("Attack");
         nextAttackTime = Time.time + attackCooldown;
+        SoundEffectManager.Play("cleaver");
     }
 
     public int getPlayerHealth() => playerHealth;
