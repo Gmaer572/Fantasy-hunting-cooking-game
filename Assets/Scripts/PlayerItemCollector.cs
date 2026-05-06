7bdcd59343cec1ab74fa29404251c8eb6c190b96
Winnie Shi <wshi@macalester.edu>
2026-05-06 18:07:06 -0500

    bug fixxing

--- Assets/Scripts/PlayerItemCollector.cs
+++ Assets/Scripts/PlayerItemCollector.cs
@@ -1,31 +1,42 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
 public class PlayerItemCollector : MonoBehaviour
 {
     private InventoryController inventoryController;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        inventoryController=FindObjectOfType<InventoryController>();
+        inventoryController = FindObjectOfType<InventoryController>();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        inventoryController = FindObjectOfType<InventoryController>();
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Item"))
+        if (!collision.CompareTag("Item")) return;
+
+        Item item = collision.GetComponent<Item>();
+        if (item == null) return;
+
+        if (inventoryController == null)
+            inventoryController = FindObjectOfType<InventoryController>();
+        if (inventoryController == null) return;
+
+        bool itemAdded = inventoryController.AddItem(collision.gameObject);
+        if (itemAdded)
         {
-            Item item = collision.GetComponent<Item>();
-            if (item != null)
-            {
-                //add item to inventory
-                bool itemAdded = inventoryController.AddItem(collision.gameObject);
-                
-                if (itemAdded)
-                {
-                    item.Pickup();
-                    Destroy(collision.gameObject);
-                }
-            }
+            item.Pickup();
+            Destroy(collision.gameObject);
         }
     }
 }
